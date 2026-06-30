9db7d801bf9d7920089873c1f8afbb74fbcca5c9
TRISTAN <tristan@SPY.kali>
2026-06-30 06:06:43 -0700

    add src modules

---
+++ src/test/java/Task_Mng/task_backend/TaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Task_Mng.task_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
